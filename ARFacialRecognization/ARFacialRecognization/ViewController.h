62aa377561682c374d1d88eca723b21978e97381
aarti <aarti.sharma@synerzip.com>
2016-09-21 12:44:14 +0530

    First commit with nslog

---
+++ ARFacialRecognization/ARFacialRecognization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARFacialRecognization
+//
+//  Created by synerzip on 21/09/16.
+//  Copyright © 2016 synerzip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ARFacialRecognization/ARFacialRecognization/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ARFacialRecognization
+//
+//  Created by synerzip on 21/09/16.
+//  Copyright © 2016 synerzip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"First git project");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
